0d10dd5430cc678f5daf0dc1aacc7713eea90ab4
AhmedEmadElDeen14 <ahmedemadeldeen0@gmail.com>
2024-07-20 23:03:19 +0300

    add all images & update AppImage file

---
+++ mobile-app/lib/core/utils/app_images.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppImages {
+  static String logo = 'assets/images/logo.png';
+  static String background = 'assets/images/background.png';
+  static String homeBackground = 'assets/images/Home_background.jpg';
+  static String footballImage = 'assets/images/football.png';
+  static String tvShowsImage = 'assets/images/TV-Shows.png';
+  static String politicalImage = 'assets/images/Political.png';
+  static String egyFlag = 'assets/images/egy-flag.png';
+  static String ukFlag = 'assets/images/UK-Flag.png';
+}
