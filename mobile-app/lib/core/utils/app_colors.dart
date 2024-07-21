527c6dbe5d3833a3d9f807312bd04fb91b73e964
YousraYousry <yousrayousry35@gmail.com>
2024-07-21 16:20:16 +0300

    update app colors file

---
+++ mobile-app/lib/core/utils/app_colors.dart
@@ -0,0 +1,58 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+class AppColor{
+  static Color Home1 =  Color(0xffD2E9FC);
+  static Color Home2 =  Color(0xffC31F93);
+  static Color CatagoriesColor1 =  Color(0xffD2E9FC);
+  static Color CatagoriesColor2 =  Color(0xffC31F93);
+  static Color CatagoriesBorder1=  Color(0xffC31F93);
+  static Color CatagoriesBorder2 =  Color(0xff882FCC);
+  static Color CatagoriesBorder3 =  Color(0xff8BDAF3);
+  static Color CatagoriesShadow =  Color(0xff000000);
+  static Color FootBallColor1 =  Color(0xff000000);
+  static Color FootBallColor2 =  Color(0xff0E031A);
+  static Color FootBallColor3 =  Color(0xff4421CD);
+  static Color FootBallColor4 =  Color(0xff0E031A);
+  static Color FootBallBorder1 =  Color(0xff2A1969);
+  static Color FootBallBorder2 =  Color(0xff8BDAF3);
+  static Color FootBallShadow =  Color(0xff000000);
+  static Color TVShowColor1 =  Color(0xff4421CD);
+  static Color TVShowColor2 =  Color(0xff000000);
+  static Color TVShowColor3 =  Color(0xff0E031A);
+  static Color TVShowBorder1 =  Color(0xff2A1969);
+  static Color TVShowBorder2 =  Color(0xff8BDAF3);
+  static Color TVShowShadow =  Color(0xff000000);
+  static Color PoliticalColor1 =  Color(0xff4421CD);
+  static Color PoliticalColor2 =  Color(0xff000000);
+  static Color PoliticalColor3 =  Color(0xff0E031A);
+  static Color PoliticalBorder1 =  Color(0xff2A1969);
+  static Color PoliticalBorder2 =  Color(0xff8BDAF3);
+  static Color PoliticalShadow =  Color(0xff000000);
+  static Color HowToPlayColor1 =  Color(0xffD9D9D9);
+  static Color HowToPlayBorder1 =  Color(0xff01BEE6);
+  static Color HowToPlayBorder2=  Color(0xff0E90C7);
+  static Color HowToPlayBorder3 =  Color(0xff1A62A9);
+  static Color HowToPlayBorder4 =  Color(0xff243E91);
+  static Color HowToPlayBorder5 =  Color(0xff2B2480);
+  static Color HowToPlayBorder6 =  Color(0xff2B2480);
+  static Color HowToPlayBorder7 =  Color(0xff310F72);
+  static Color HowToPlayBorder8 =  Color(0xff301173);
+  static Color HowToPlayBorder9 =  Color(0xff2E1878);
+  static Color HowToPlayBorder10 =  Color(0xff2B2580);
+  static Color HowToPlayBorder11 =  Color(0xff26378C);
+  static Color HowToPlayBorder12 =  Color(0xff204E9C);
+  static Color HowToPlayBorder13 =  Color(0xff186AAE);
+  static Color HowToPlayBorder14 =  Color(0xff0F8CC5);
+  static Color HowToPlayBorder15 =  Color(0xff04B2DE);
+  static Color HowToPlayBorder16 =  Color(0xff01BEE6);
+  static Color HowToPlayShadow =  Color(0xffC0FCFF);
+  static Color Settings1 =  Color(0xffD2E9FC);
+  static Color Settings2 =  Color(0xffC31F93);
+  static Color language =  Color(0xffFFFFFF);
+  static Color Continue1 =  Color(0xff3C1699);
+  static Color Continue2 =  Color(0xff128DD0);
+  static Color Continue3 =  Color(0xff3B189A);
+
+
+}
