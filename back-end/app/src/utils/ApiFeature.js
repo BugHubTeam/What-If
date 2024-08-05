14933992588662e45d9e6a7e0e886e7250ed05f1
omerbag-9 <omerbagprog@gmail.com>
2024-08-05 17:45:07 +0300

    feat Api feature

---
+++ back-end/app/src/utils/ApiFeature.js
@@ -0,0 +1,33 @@
+export class ApiFeature {
+    constructor(mongooseQuery, queryData) {
+        this.mongooseQuery = mongooseQuery
+        this.queryData = queryData
+    }
+    pagination() {
+    let {page , size} = this.queryData
+        page = parseInt(page)  
+        size = parseInt(size) 
+        if (page <= 0) page = 1
+        if (size <= 0) size = 2
+        const skip = (page - 1) * size
+        this.mongooseQuery.limit(size).skip(skip)
+        return this
+    }
+
+    sort(){
+        this.mongooseQuery.sort(this.queryData.sort?.replaceAll(',' , " "))
+        return this
+    }
+
+    select(){
+        this.mongooseQuery.select(this.queryData.select?.replaceAll(',' , " "))
+        return this
+    }
+
+    filter(){
+        let {page , size , sort , select , ...filter} = this.queryData
+        filter = JSON.parse(JSON.stringify(filter).replace(/get|gte|lt|lte/g , match => `$${match}`))
+        this.mongooseQuery.find(filter)
+        return this
+    }
+}
