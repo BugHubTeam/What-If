604303cecb49e486b589f4e7afecf008e771fe76
omerbag-9 <omerbagprog@gmail.com>
2024-08-05 17:53:52 +0300

    implement api feature for category and football subcategories

--- back-end/app/src/modules/category/category.controller.js
+++ back-end/app/src/modules/category/category.controller.js
@@ -1,6 +1,8 @@
 import { Category } from "../../../db/models/category.model.js"
+import { ApiFeature } from "../../utils/ApiFeature.js"
 
 export const getCategories = async (req, res, next) => {
-    const categories = await Category.find()
+    const apiFeature = new ApiFeature(Category.find(), req.query).pagination().select().filter().sort()
+    const categories = await apiFeature.mongooseQuery
     return res.status(200).json({ data: categories })
 }
