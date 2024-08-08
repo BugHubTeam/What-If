13afd078d196f88e1736dc9c2c47abb658677368
Loaa17 <loaaabdelmonem@gmail.com>
2024-08-08 15:13:10 +0300

    init app(routing)+navbar(without design)

---
+++ front-end/whatIf/src/Components/Layout/Layout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import Footer from '../Footer/Footer'
+import { Outlet } from 'react-router-dom'
+
+
+
+export default function Layout() {
+  return (
+    <>
+
+    <Navbar/>
+    
+  <div className="container mx-auto py-10">
+    <Outlet/>
+  </div>
+    
+    <Footer/>
+    
+    </>
+  )
+}
