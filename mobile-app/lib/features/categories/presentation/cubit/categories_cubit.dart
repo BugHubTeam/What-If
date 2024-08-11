4ada303e184f24ff230e514e50d46448db0b18d0
Ahmed Emad ElDeen <ahmmedemmad2@gmail.com>
2024-08-11 14:47:40 +0300

    Revert "Mostafa dev"

--- mobile-app/lib/features/categories/presentation/cubit/categories_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:equatable/equatable.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-part 'categories_state.dart';
-
-class CategoriesCubit extends Cubit<CategoriesState> {
-  CategoriesCubit() : super(CategoriesInitial());
-}
